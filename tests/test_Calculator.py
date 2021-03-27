d85b3890d9ae27aa832fcc70ee814624d4b2aa17
Chinonso Anyanka <cc792@njit.edu>
2021-03-27 10:58:13 -0400

    Feature: added test files

---
+++ tests/test_Calculator.py
@@ -0,0 +1,107 @@
+import unittest
+
+from src.calculator import Calculator
+
+from tests.src.CSVReader import readCSV
+
+class TestCalculator(unittest.TestCase):
+    # this is the Calculator class instance.
+    calculator = None
+
+    # class level setup function, execute once only before any test function.
+    @classmethod
+    def setUpClass(cls):
+        print('')
+        print('setUpClass')
+
+    # class level setup function, execute once only after all test function's execution.
+    @classmethod
+    def tearDownClass(cls):
+        print('')
+        print('tearDownClass')
+
+    # execute before every test case function run.
+    def setUp(self):
+        self.calculator = Calculator()
+        print('')
+        print('setUp')
+
+    # execute after every test case function run.
+    def tearDown(self):
+        # release the Calculator object.
+        if self.calculator is not None:
+            self.calculator = None
+        print('')
+        print('tearDown')
+
+    # below are function that test Calculator class's plus, minus, multiple and divide functions.
+    def testInstantiateCalculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def testAddition(self):
+        rows = readCSV("tests/testCases/Addition.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertEqual(self.calculator.add(x, y), expectedResult)
+
+    def testSubtraction(self):
+        rows = readCSV("tests/testCases/Subtraction.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertEqual(self.calculator.subtract(x, y), expectedResult)
+
+    def testDivision(self):
+        self.assertRaises(ZeroDivisionError, self.calculator.divide, 1, 0)
+        rows = readCSV("tests/testCases/Division.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            y = float(row["Value 1"])
+            x = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.divide(x, y), expectedResult)
+
+    def testMultiplication(self):
+        rows = readCSV("tests/testCases/Multiplication.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            y = float(row["Value 2"])
+            self.assertAlmostEqual(self.calculator.multiply(x, y), expectedResult)
+
+    def testSquare(self):
+        rows = readCSV("tests/testCases/Square.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.square(x), expectedResult)
+
+    def testSquareRoot(self):
+        rows = readCSV("tests/testCases/SquareRoot.csv")
+        for row in rows:
+            expectedResult = float(row["Result"])
+            x = float(row["Value 1"])
+            self.assertAlmostEqual(self.calculator.squareRoot(x), expectedResult)
+
+    def testResultProperty(self):
+        self.calculator.results.clear()
+        self.assertEqual(self.calculator.results, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+def build_test_suite():
+    # create unittest.TestSuite object.
+    test_suite = unittest.TestSuite()
+    # add each test function to the test suite object.
+    test_suite.addTest(TestCalculator('test_addition'))
+    test_suite.addTest(TestCalculator('test_subtraction'))
+    test_suite.addTest(TestCalculator('test_multiplication'))
+    test_suite.addTest(TestCalculator('test_division'))
+    test_suite.addTest(TestCalculator('test_squareRoot'))
+    test_suite.addTest(TestCalculator('test_squared'))
+    return test_suite
---
+++ tests/test_Pop_Sample.py
@@ -0,0 +1,60 @@
+import unittest
+import random
+
+from src.pop_sample_calculator import Population_Sampling_Calculator
+
+seedNum = 50  # KEEP AT 50 because values are checked against this seed
+random.seed(seedNum)
+original_list = []
+for x in range(50):
+    original_list.append(float(random.randint(40, 50)))
+
+
+class SamplingTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calc_obj = Population_Sampling_Calculator()
+
+    def testInstantiateCalculator(self):
+        self.assertIsInstance(self.calc_obj, Population_Sampling_Calculator)
+
+    def test_random_sampling(self):
+        print("******test_random_sampling******")
+        sample_list = self.calc_obj.random_sampling(original_list, 8)
+        print("Random Sample List")
+        print(sample_list, "\n")
+        self.assertEqual(sample_list, [49.0, 44.0, 42.0, 46.0, 43.0, 47.0, 50.0, 49.0])
+        # [41.0, 50.0, 47.0, 43.0, 42.0, 49.0, 47.0, 43.0]
+
+    def test_confidence_interval_for_sample(self):
+        print("******test_confidence_interval******")
+        sample_list = self.calc_obj.random_sampling(original_list, len(original_list))
+        result = self.calc_obj.confidence_interval_for_sample(0.9, sample_list)
+        print("Confidence Interval for Sample List")
+        print(result, "\n")
+        self.assertAlmostEqual(result, (44.43924576646906, 45.840754233530944))
+
+    def test_margin_error(self):
+        print("******test_margin_error******")
+        res = self.calc_obj.margin_error(original_list)
+        print("Margin of Error:", res, "\n")
+        self.assertEqual(res, 0.41797373220852824)
+
+    def test_sample_size(self):
+        print("******test_sample_size******")
+        res = self.calc_obj.sample_size(original_list)
+        print("Sample Size:", res, "\n")
+        self.assertEqual(res, 25)
+
+    def test_cochrans(self):
+        print("******test_cochrans******")
+        res = self.calc_obj.cochrans(original_list)
+        print("Cochrans' Sample Size:", res, "\n")
+        self.assertEqual(res, 5)
+
+    def testResultProperty(self):
+        self.calc_obj.results.clear()
+        self.assertEqual(self.calc_obj.results, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
